5d5010eb87f4fd4e7cb0ba184ccff1a6bb9f2805
KristofferEklund1 <56302895+KristofferEklund1@users.noreply.github.com>
2020-01-21 10:49:57 +0100

    Module asg
    enjoy

---
+++ Programming Foundations 2/js.js
@@ -0,0 +1,81 @@
+
+// question 1
+
+var petArray = ["cat", "cow", "dog",];
+
+// question 2
+
+console.log(petArray[i]);
+
+
+// question 3
+
+(petArray.push("sheep"));
+
+// question 4
+
+var numberOfpetArray = petArray.length;
+console.log(numberOfpetArray)
+
+// question 5
+
+var catObjeect = [
+    {
+        name: "Guri",
+        color: "grey",
+        age: "3"
+    },
+    {
+        name: "Kalle",
+        color: "black",
+        age: "18"
+    },
+    {
+        name: "Turi",
+        color: "white",
+        age: "8"
+    },
+   
+];
+
+// question 6
+
+console.log(catObjeect.age)
+
+// question 7
+
+var catArray = [
+    {
+    name: "Guri",
+    color: "grey",
+    age: "3"
+    },
+    {
+    name: "Kalle",
+    color: "black",
+    age: "18"
+    },
+    {
+    name: "Turi",
+    color: "white",
+    age: "8"
+    },
+];
+
+// question 8
+
+for(var i = 0; i < catArray.length; i++) {
+
+    console.log(catArray[i]);
+}
+
+// question 9
+function logToConsole(pets){
+console.log(pets)
+}
+
+
+// question 10
+
+// I didn't understand this question could you help me in guidance..
+
