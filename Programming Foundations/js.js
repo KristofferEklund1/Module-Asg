00d332bc9a44a1cd7b24201d35bc698f89a84194
KristofferEklund1 <56302895+KristofferEklund1@users.noreply.github.com>
2020-01-23 14:48:10 +0100

    Delivery: Programming Foundations MA1

---
+++ Programming Foundations/js.js
@@ -0,0 +1,32 @@
+// Question 1
+firstVar = "";
+
+//Question 2
+var name = "kristoffer";
+
+//Question 3
+var number = 5;
+
+//Queston 4
+var division = 20 / 5;
+
+//Queston 5
+typeoff (frog);
+
+
+//Queston 6 and 7
+var orderHasShipped = true;
+
+if (orderHasShipped === true) {
+    console.log ("The order has shipped")
+}
+
+else {
+    console.log("The order has not been shipped")
+}
+
+//Queston 8
+for (var count = 1; count <= 9; count ++) {
+    console.log(count)
+}
+
