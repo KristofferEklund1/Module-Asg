96bc4102ab917656f9476d59de93f3ac7ff1584e
KristofferEklund1 <56302895+KristofferEklund1@users.noreply.github.com>
2020-02-06 12:52:27 +0100

    Finish :)

---
+++ js1-ma2/js1-ma2/js.js
@@ -0,0 +1,14 @@
+// Question 1
+
+const myFuncstionExpression = function (){
+    console.log("Kriss")
+
+}
+
+
+// Question 2
+
+document.querySelector(".btn");
+someButton.addEventListener("click");
+
+
